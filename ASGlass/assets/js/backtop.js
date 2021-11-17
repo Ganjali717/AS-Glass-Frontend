4a109340bf4c1f711add310f213f4c29b79452f1
Ganjali717 <ganjaliki@code.edu.az>
2021-11-17 17:16:32 +0400

    Contact and some pages is ready

---
+++ ASGlass/assets/js/backtop.js
@@ -0,0 +1,24 @@
+var mybutton = document.getElementById("backtop");
+var nav = document.getElementById("menu");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    nav.style.position = "fixed";
+    nav.style.top = 0;
+  } else {
+    mybutton.style.display = "none";
+    nav.style.position = "relative";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
